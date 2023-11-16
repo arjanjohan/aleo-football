d02fd71a8c20f54cd3cde26cc232b5123f0bcdbe
arjanjohan <arjanvanhoogdalem@gmail.nl>
2023-11-16 19:49:56 +0100

    rename and add alex leo files

---
+++ game_logic_v0001/deploy.sh
@@ -0,0 +1,13 @@
+WALLETADDRESS=""
+PRIVATEKEY="APrivateKey1zkp31BfbXfMVqt9a8cTUknAit6BDSQwkJ1ssLhAui2tTDQ3"
+
+APPNAME="game_logic"
+PATHTODAPP=${realpath -q $APPNAME}
+
+RECORD = "{
+
+}"
+
+cd ..&& 
+
+snarkos developer deploy game_logic.aleo --private-key "APrivateKey1zkp31BfbXfMVqt9a8cTUknAit6BDSQwkJ1ssLhAui2tTDQ3"
